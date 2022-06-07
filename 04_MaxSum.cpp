6993f87d13557480e5551b012581ec187f6b074f
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-07 13:17:32 +0530

    Solution 1: Recursion

--- 04_MaxSum.cpp
+++ 04_MaxSum.cpp
@@ -8,6 +8,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Using recursion to find all the valid sub-Sequence
+public:
+    int maximumNonAdjacentSum(vector<int> &nums)
+    {
+        int sum = 0, ans = -1;
+        subSum(nums, sum, ans);
+        return ans;
+    }
+    void subSum(vector<int> &nums, int &sum, int &ans, int i = 0)
+    {
+        if (i >= nums.size())
+        {
+            ans = max(ans, sum);
+            return;
+        }
+        sum += nums[i];
+        subSum(nums, sum, ans, i + 2);
+        sum -= nums[i];
+        subSum(nums, sum, ans, i + 1);
+    }
+};
+
 class Solution
 {
     // Tabulation
@@ -16,7 +40,7 @@ public:
     {
         if (nums.size() == 1)
             return nums[0];
-        else if (nums.size() == 2) 
+        else if (nums.size() == 2)
             return max(nums[0], nums[1]);
         vector<int> dp(nums.size());
         dp[0] = nums[0], dp[1] = nums[1];
@@ -36,6 +60,9 @@ public:
 
 int main()
 {
+    Solution1 obj1;
+    vector<int> nums = {9, 9, 5, 7, 2, 0, 3};
+    cout << obj1.maximumNonAdjacentSum(nums);
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
