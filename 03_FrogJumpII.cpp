7174fd4556f47fb696fb47cd2ce2cdd626fe246c
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-07 12:16:31 +0530

    Solution 1: Memoization

--- 03_FrogJumpII.cpp
+++ 03_FrogJumpII.cpp
@@ -3,9 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Memoization (Top down)
+public:
+    int frogJump(int n, vector<int> &heights, int k)
+    {
+        vector<int> dp(n, -1);
+        return dp[n-1] = solve(n - 1, heights, k, dp);
+    }
+    int solve(int n, vector<int> &heights, int &k, vector<int> &dp)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int subMin = INT_MAX;
+        for (int i = 1; i <= k; ++i)
+        {
+            int f = INT_MAX;
+            if (n >= i)
+            {
+                f = solve(n - i, heights, k, dp) + abs(heights[n] - heights[n - i]);
+            }
+            subMin = min(subMin, f);
+        }
+        return dp[n] = subMin;
+    }
+};
+
 class Solution
 {
-    // Solution 1 : Simple recursion 
+    // Simple recursion 
 public:
     int frogJump(int n, vector<int> &heights, int k)
     {
@@ -34,7 +66,7 @@ public:
 int main()
 {
     vector<int> heights = {10, 20, 30, 10};
-    Solution Obj1;
+    Solution1 Obj1;
     cout << Obj1.frogJump(4, heights, 2);
 
     ios_base::sync_with_stdio(false);
