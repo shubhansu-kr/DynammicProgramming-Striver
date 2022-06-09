dadcfed7f8b0d6e080b13f0503e71cc63aa62f71
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-09 13:58:26 +0530

    Find the number of unique path

---
+++ 09_UniquePath.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/unique-paths/
+
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner
+// (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1]
+// [n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the
+// robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        int ans = 0;
+        solve(m - 1, n - 1, ans);
+        return ans;
+    }
+    void solve(int m, int n, int &ans)
+    {
+        if (m < 0 || n < 0)
+            return;
+        if (m == 0 && n == 0)
+        {
+            ++ans;
+            return;
+        }
+        solve(m - 1, n, ans);
+        solve(m, n - 1, ans);
+    }
+};
+
+int main()
+{
+    Solution Obj1;
+    cout << Obj1.uniquePaths(3, 7);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
