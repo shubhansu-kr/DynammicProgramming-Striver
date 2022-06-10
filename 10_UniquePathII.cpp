4934569704a8120695b8dc7d5d25e18f6212e0b0
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-10 16:03:13 +0530

    SOlution 3 : Tabulation

--- 10_UniquePathII.cpp
+++ 10_UniquePathII.cpp
@@ -17,6 +17,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution3
+{
+    // Tabulation
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (obstacleGrid[i][j])
+                {
+                    dp[i][j] = 0;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        if (j > 0) dp[i][j] = dp[i][j - 1];
+                    }
+                    else if (j == 0 && i > 0)
+                    {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
 class Solution2
 {
     // Recursion : Memoization
@@ -100,7 +139,7 @@ int main()
 {
     vector<vector<int>> grid = {{0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}};
 
-    Solution2 Obj1;
+    Solution3 Obj1;
     cout << Obj1.uniquePathsWithObstacles(grid);
 
     ios_base::sync_with_stdio(false);
