019fb7bafdf458a5d17f6a449484ac2c23cd691e
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-17 15:20:23 +0530

    Solution 3: Memoization

--- 17_EqualPartition.cpp
+++ 17_EqualPartition.cpp
@@ -7,6 +7,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution3
+{
+    // Recursion Optimised : Memoization
+
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = 0, n = nums.size();
+        for (int a : nums)
+            sum += a;
+        vector<vector<int>> dp(n+1, vector<int>(sum / 2+1, -1));
+        return (sum % 2) ? false : solve(nums, dp, sum / 2, n - 1);
+    }
+
+    bool solve(vector<int> &nums, vector<vector<int>> &dp, int k, int i)
+    {
+        if (k == 0)
+            return true;
+        if (i == 0)
+            return k == nums[i];
+
+        if (dp[i][k] != -1)
+            return dp[i][k];
+
+        // Pick
+        bool pick = false;
+        if (nums[i] <= k)
+            pick = solve(nums, dp, k - nums[i], i - 1);
+        if (pick)
+            return pick;
+        // NotPick
+        bool noPick = solve(nums, dp, k, i - 1);
+
+        return dp[i][k] = (pick || noPick);
+    }
+};
+
 class Solution2
 {
     // Recursion Optimised :
@@ -35,7 +72,7 @@ public:
             return pick;
         // NotPick
         bool noPick = solve(nums, k, i - 1);
-    
+
         return (pick || noPick);
     }
 };
