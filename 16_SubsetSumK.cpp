dab336e9842ce872b7450add113f43e8daa5a5b2
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-17 11:39:50 +0530

    Check if subSeq with sum k exists

---
+++ 16_SubsetSumK.cpp
@@ -0,0 +1,61 @@
+// https://www.codingninjas.com/codestudio/problems/subset-sum-equal-to-k_1550954
+
+// You are given an array/list ‘ARR’ of ‘N’ positive integers and an integer
+// ‘K’. Your task is to check if there exists a subset in ‘ARR’ with a sum
+// equal to ‘K’.
+
+// Note: Return true if there exists a subset with sum equal to ‘K’. Otherwise,
+// return false.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // Simple recursive solution
+public:
+    bool subsetSumToK(int n, int k, vector<int> &arr)
+    {
+        return solve(arr, k, n - 1);
+    }
+
+    bool solve(vector<int> &arr, int &k, int n)
+    {
+        if (n < 0 || k <= 0)
+        {
+            if (k == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // pick element
+        k -= arr[n];
+        if (solve(arr, k, n - 1))
+        {
+            return true;
+        }
+
+        // No pick
+        k += arr[n];
+        if (solve(arr, k, n - 1))
+        {
+            return true;
+        }
+        return false ;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4};
+    int k = 0;
+
+    Solution Obj1;
+    cout << Obj1.subsetSumToK(4, k, arr);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
