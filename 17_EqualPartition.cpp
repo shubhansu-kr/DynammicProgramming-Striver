690a55d9a174f9d4147bf3d9bfdc1ebcc902921f
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-17 14:59:05 +0530

    SOlution 1: Memoization

--- 17_EqualPartition.cpp
+++ 17_EqualPartition.cpp
@@ -7,6 +7,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Recursive solution : Memoization
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int right = 0, left = 0;
+        for (int a : nums)
+            right += a;
+        vector<vector<int>> dp(nums.size(), vector<int>(right + 1, -1));
+        return solve(nums, dp, left, right);
+    }
+    bool solve(vector<int> &nums, vector<vector<int>> &dp, int left, int right, int i = 0)
+    {
+        // Base condition
+        if (left == right)
+            return true;
+        if (i == nums.size())
+            return left == right;
+
+        if (dp[i][left] != -1)
+            return dp[i][left];
+        // Take
+        bool take = false;
+        if (nums[i] + left <= right)
+            take = solve(nums, dp, left + nums[i], right - nums[i], i + 1);
+
+        if (take)
+            return take;
+
+        // noTake
+        bool noTake = solve(nums, dp, left, right, i + 1);
+
+        return dp[i][left] = (take || noTake);
+    }
+};
+
 class Solution
 {
     // Recursive solution : TLE
