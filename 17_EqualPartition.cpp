4eb6b59e3e969719fb5d81d29c02e0c1498b7094
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-17 14:53:53 +0530

    Check if Array can be equally divided

---
+++ 17_EqualPartition.cpp
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/partition-equal-subset-sum/
+
+// Given a non-empty array nums containing only positive integers,
+// find if the array can be partitioned into two subsets such that
+// the sum of elements in both subsets is equal.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // Recursive solution : TLE
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int right = 0, left = 0;
+        for (int a : nums)
+            right += a;
+        return solve(nums, left, right);
+    }
+    bool solve(vector<int> &nums, int left, int right, int i = 0)
+    {
+        // Base condition
+        if (left == right)
+            return true;
+        if (i == nums.size())
+            return left == right;
+
+        // Take
+        bool take = false;
+        if (nums[i] + left <= right)
+            take = solve(nums, left + nums[i], right - nums[i], i + 1);
+
+        if (take)
+            return take;
+
+        // noTake
+        bool noTake = solve(nums, left, right, i + 1);
+
+        return (take || noTake);
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1, 9, 3, 5};
+
+    Solution Obj1;
+    cout << Obj1.canPartition(nums);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
