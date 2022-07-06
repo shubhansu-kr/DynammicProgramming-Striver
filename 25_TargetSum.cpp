fe4f58e489256a994c96637b725411d2d763f56d
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-06 16:38:07 +0530

    Solution 2: Subset sum

--- 25_TargetSum.cpp
+++ 25_TargetSum.cpp
@@ -12,6 +12,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
+class Solution2
+{
+    // Recursion : subset SUm
+public:
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int sumNums = accumulate(nums.begin(), nums.end(), 0);
+        target = (sumNums + target) / 2;
+        return solve(nums, target, n - 1);
+    }
+    int solve(vector<int> nums, int target, int i)
+    {
+        if (i == 0)
+            return nums[i] == target;
+        if (target == 0)
+            return 1;
+
+        // Pick : Dont pick if nums[i] > target
+        int p = 0;
+        if (nums[i] <= target)
+            p = solve(nums, target - nums[i], i - 1);
+        // noPick
+        int np = solve(nums, target, i - 1);
+
+        return  p + np;
+    }
+};
+
 class Solution1
 {
     // Recursion : Optimised
@@ -76,7 +107,7 @@ int main()
     vector<int> nums = {1, 1, 1, 1, 1};
     int target = 3;
 
-    Solution1 Obj1;
+    Solution2 Obj1;
     cout << Obj1.findTargetSumWays(nums, target);
 
     ios_base::sync_with_stdio(false);
