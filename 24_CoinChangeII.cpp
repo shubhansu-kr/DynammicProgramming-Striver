671e0f88a5b60bd8795bc647fbf9138e8921f069
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-06 14:36:50 +0530

    SOlution 2: Memoization

--- 24_CoinChangeII.cpp
+++ 24_CoinChangeII.cpp
@@ -10,6 +10,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution2
+{
+    // Recursion: <Memoization
+public:
+    int change(int amount, vector<int> &coins)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return solve(coins, dp, amount, n - 1);
+    }
+    int solve(vector<int> &coins, vector<vector<int>> &dp, int amount, int n)
+    {
+        // base condition
+        if (amount == 0) return 1;
+        if (n == -1) return 0;
+
+        if (dp[n][amount] != -1) return dp[n][amount];
+
+        // pick
+        int pick = 0;
+        if (amount >= coins[n])
+            pick = solve(coins, dp, amount - coins[n], n);
+        // noPick
+        int noPick = solve(coins, dp, amount, n - 1);
+
+        return dp[n][amount] = pick + noPick;
+    }
+};
+
 class Solution1
 {
     // Recursion: Optimised
@@ -26,7 +55,7 @@ public:
             return 1;
         if (n == -1)
             return 0;
-            
+
         // pick
         int pick = 0;
         if (amount >= coins[n])
