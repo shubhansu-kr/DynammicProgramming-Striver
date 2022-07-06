8c0ec3aceadd795de52f5565748a892796018102
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-06 14:55:18 +0530

    SOlution 3: Tabulation

--- 24_CoinChangeII.cpp
+++ 24_CoinChangeII.cpp
@@ -10,6 +10,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution3
+{
+    // Tabulation
+public:
+    int change(int amount, vector<int> &coins)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1));
+
+        for (int amt = 1; amt <= amount; ++amt)
+        {
+            if (amt % coins[0] == 0) dp[0][amt] = 1;
+            else dp[0][amt] = 0;
+        }
+
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 1; j <= amount; ++j)
+            {
+                int pick = 0;
+                if (j >= coins[i]) pick = dp[i][j - coins[i]];
+                int noPick = dp[i - 1][j];
+                dp[i][j] = pick + noPick;
+            }
+        }
+        return dp[n - 1][amount];
+    }
+};
+
 class Solution2
 {
     // Recursion: <Memoization
@@ -23,10 +52,13 @@ public:
     int solve(vector<int> &coins, vector<vector<int>> &dp, int amount, int n)
     {
         // base condition
-        if (amount == 0) return 1;
-        if (n == -1) return 0;
+        if (amount == 0)
+            return 1;
+        if (n == -1)
+            return 0;
 
-        if (dp[n][amount] != -1) return dp[n][amount];
+        if (dp[n][amount] != -1)
+            return dp[n][amount];
 
         // pick
         int pick = 0;
