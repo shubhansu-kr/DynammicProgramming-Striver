fc936b09f50f8f67cf886a763ce979256e2fbb1f
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-09 18:07:11 +0530

    SOlution 2: Tabulation

--- 28_LongestCommonSubSeq.cpp
+++ 28_LongestCommonSubSeq.cpp
@@ -12,9 +12,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution2
+{
+    // Tabulation 
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1, vector<int>(n2));
+
+        int ind = text2.find(text1[0]);
+        if (ind != string::npos) for (int i = ind; i < n2; ++i) dp[0][i] = 1;
+        ind = text1.find(text2[0]);
+        if (ind != string::npos) for (int i = ind; i < n1; ++i) dp[i][0] = 1;
+
+        for (int i = 1; i < n1; ++i)
+            for (int j = 1; j < n2; ++j)
+                if (text1[i] == text2[j])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else         
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+
+        return dp[n1-1][n2-1];
+    }
+};
+
 class Solution1
 {
-    // Recursion: Memoization 
+    // Recursion: Memoization : TLE
 public:
     int longestCommonSubsequence(string text1, string text2)
     {
@@ -29,16 +54,17 @@ public:
             return 0;
 
         // Dp
-        if (dp[n1][n2] != -1) return dp[n1][n2];
+        if (dp[n1][n2] != -1)
+            return dp[n1][n2];
 
         // Match
         if (s1[n1] == s2[n2])
         {
-            return 1 + solve(s1, s2, dp, n1 - 1, n2 - 1);
+            return dp[n1][n2] = 1 + solve(s1, s2, dp, n1 - 1, n2 - 1);
         }
         else
         {
-            return max(solve(s1, s2, dp, n1, n2 - 1), solve(s1, s2, dp, n1 - 1, n2));
+            return dp[n1][n2] = max(solve(s1, s2, dp, n1, n2 - 1), solve(s1, s2, dp, n1 - 1, n2));
         }
     }
 };
@@ -73,9 +99,9 @@ public:
 int main()
 {
 
-    string text1 = "bdf", text2 = "ace";
+    string text1 = "bda", text2 = "ace";
 
-    Solution Obj1;
+    Solution1 Obj1;
     cout << Obj1.longestCommonSubsequence(text1, text2);
 
     ios_base::sync_with_stdio(false);
