93fe1e26ded07516f2ad9be7ce2c1d47258892b4
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-10 15:57:29 +0530

    Print the longest common subsequence

--- 30_LongestCommonSubstring.cpp
+++ 30_LongestCommonSubstring.cpp
@@ -8,6 +8,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Print the longest common substring
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+
+        int ans = INT_MIN;
+        int x, y;
+        for (int i = 1; i <= n1; ++i)
+        {
+            for (int j = 1; j <= n2; ++j)
+            {
+                if (text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = 0;
+                
+                if (ans < dp[i][j]) {
+                    ans = dp[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        int len = ans;
+        string lcs(len, '_');
+
+        while (x && y && dp[x][y]) {
+            lcs[--len] = text1[x-1];
+            --x, --y;
+        }
+        
+        cout << lcs;
+        return 0; 
+    }
+};
+
 class Solution
 {
 public:
@@ -37,8 +76,8 @@ int main()
 
     string text1 = "apple", text2 = "rampageapple";
 
-    Solution Obj1;
-    cout << Obj1.longestCommonSubsequence(text1, text2);
+    Solution1 Obj1;
+    Obj1.longestCommonSubsequence(text1, text2);
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
