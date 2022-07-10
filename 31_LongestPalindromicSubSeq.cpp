fad35c56f906b5857787869a532c7a150cba1ca0
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-10 16:51:46 +0530

    Soltuion1: Memoization

--- 31_LongestPalindromicSubSeq.cpp
+++ 31_LongestPalindromicSubSeq.cpp
@@ -3,6 +3,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Recursion: Memoization 
+public:
+    int longestPalindromeSubseq(string s)
+    {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1)); 
+        return solve(s,dp, 0, n - 1);
+    }
+    int solve(string &s, vector<vector<int>> &dp, int i, int j)
+    {
+        if (i == j) return 1;
+        if (i > j) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == s[j]) return dp[i][j] = 2 + solve(s,dp, i + 1, j - 1);
+        else return dp[i][j] = max(solve(s,dp, i + 1, j), solve(s,dp, i, j - 1));
+    }
+};
+
 class Solution
 {
     // BruteForce: Recursion  
