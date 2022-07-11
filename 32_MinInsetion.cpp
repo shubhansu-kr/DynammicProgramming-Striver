23b81780493a6e2eddd12ff3d8b519e13bca4996
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-11 16:29:17 +0530

    Solution1: Memoization

--- 32_MinInsetion.cpp
+++ 32_MinInsetion.cpp
@@ -9,6 +9,22 @@
 using namespace std ;
 
 class Solution {
+    // Recursion: Memoization
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1)); 
+        return solve(s, dp, 0, n-1);
+    }
+    int solve(string &s, vector<vector<int>> &dp, int i, int j) {
+        if (i >= j) return 0 ;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s[i] == s[j]) return dp[i][j] = solve(s, dp, i+1, j-1);
+        else return dp[i][j] = 1 + min(solve(s, dp, i+1, j), solve(s, dp, i, j-1));
+    }
+};
+
+class Solution {
     // BruteForce: Recursion
 public:
     int minInsertions(string s) {
