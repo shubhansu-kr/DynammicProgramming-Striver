6c84155addc7ef197e8e1cffc27e0688a8be1b83
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-11 17:42:31 +0530

    Solution2: Tabulation

--- 33_Mindeletiontoequalisestring.cpp
+++ 33_Mindeletiontoequalisestring.cpp
@@ -7,6 +7,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution2
+{
+    // Tabulation 
+public:
+    int minDistance(string word1, string word2) {
+        int  n1 = word1.size(), n2 = word2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int> (n2 + 1, 0));     
+        for (int i = 1; i <= n1; ++i)
+            for (int j = 1; j <= n2; ++j) 
+                if (word1[i-1] == word2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);       
+        int lcs = dp[n1][n2];
+        return (n1 + n2 - 2*lcs);
+    }
+};
+
 class Solution1
 {
     // Recursion: Memoization  
