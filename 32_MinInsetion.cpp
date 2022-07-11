eb0da6c5a53d58ba63ea51cf47654ce12761606a
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-11 16:36:12 +0530

    Solution2: Tabulation

--- 32_MinInsetion.cpp
+++ 32_MinInsetion.cpp
@@ -8,7 +8,26 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
-class Solution {
+class Solution2 {
+    // Tabulation 
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n)); 
+
+        int x = 1;
+        while (x < n)
+        {
+            for (int i = 0, j = x; i < n && j < n; ++i, ++j)
+                if (s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1];
+                else dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+            ++x ;
+        }
+        return dp[0][n - 1];
+    }
+};
+
+class Solution1 {
     // Recursion: Memoization
 public:
     int minInsertions(string s) {
