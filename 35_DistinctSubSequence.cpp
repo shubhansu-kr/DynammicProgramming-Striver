b39a84cd5b2cff5048845d85d2736e077183c759
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-28 17:30:38 +0530

    Wrong answer

--- 35_DistinctSubSequence.cpp
+++ 35_DistinctSubSequence.cpp
@@ -3,6 +3,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution5
+{
+    // Tabulation: Wrong Answer  
+public:
+    int numDistinct(string s, string t)
+    {
+        if (s.length() <= t.length()) return s == t;
+        vector<vector<int>> dp(s.length()+1, vector<int> (t.length()+1));        
+
+        for (int i = 1; i <= s.length(); ++i)
+        {
+            for (int j = 1; j <= t.length(); ++j)
+            {
+                if (s[i-1] == t[j-1]) return dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else return dp[i][j] = dp[i-1][j];
+            }   
+        }
+        return dp[s.length()][t.length()];
+    }
+
+};
+
 class Solution4
 {
     // Recursion: Memoization: Bottom up  
@@ -134,7 +156,7 @@ int main()
 {
     string s = "babgbag", t = "bag";
 
-    Solution4 Obj1;
+    Solution5 Obj1;
     cout << Obj1.numDistinct(s, t);
 
     ios_base::sync_with_stdio(false);
