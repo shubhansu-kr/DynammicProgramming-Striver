1be0d6bb29121d8419358d180a4c5da7427959f7
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-28 18:03:13 +0530

    Distinct SubSequence

--- 35_DistinctSubSequence.cpp
+++ 35_DistinctSubSequence.cpp
@@ -10,17 +10,18 @@ public:
     int numDistinct(string s, string t)
     {
         if (s.length() <= t.length()) return s == t;
-        vector<vector<int>> dp(s.length()+1, vector<int> (t.length()+1));        
+        vector<vector<double>> dp(s.length()+1, vector<double> (t.length()+1, 0));        
 
+        for (int i = 0; i <= s.length(); ++i) dp[i][0] = 1;        
         for (int i = 1; i <= s.length(); ++i)
         {
             for (int j = 1; j <= t.length(); ++j)
             {
-                if (s[i-1] == t[j-1]) return dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                else return dp[i][j] = dp[i-1][j];
+                if (s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
             }   
         }
-        return dp[s.length()][t.length()];
+        return (int)dp[s.length()][t.length()];
     }
 
 };
