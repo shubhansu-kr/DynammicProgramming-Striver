14332f2e74788665b19ed3c5702aa08cdcdb1f2e
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-28 16:24:15 +0530

    SOlution 1: Recursion optimised

--- 35_DistinctSubSequence.cpp
+++ 35_DistinctSubSequence.cpp
@@ -3,13 +3,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Recursion
+public:
+    int numDistinct(string s, string t)
+    {
+        if (s.length() <= t.length()) return s.length() == t.length();
+
+        int ans = 0;
+        string sub;
+        return solve(s, t, sub, ans);
+    }
+
+    int solve(string &s, string &t, string &sub, int i = 0)
+    {
+        if (i == s.length())
+        {
+            if (sub == t) return 1;
+            return 0;
+        }
+
+        // Pick
+        sub.push_back(s[i]);
+        int pick = solve(s, t, sub, i + 1);
+        sub.pop_back();
+
+        // Dont Pick
+        int noPick = solve(s, t, sub, i + 1);
+
+        return pick + noPick;
+    }
+};
+
 class Solution
 {
     // BruteForce: Recursion
 public:
     int numDistinct(string s, string t)
     {
-        if (s.length() <= t.length()) return s.length() == t.length() ;
+        if (s.length() <= t.length())
+            return s.length() == t.length();
 
         int ans = 0;
         string sub;
@@ -20,25 +54,26 @@ public:
     {
         if (i == s.length())
         {
-            if (sub == t) ++ans;
+            if (sub == t)
+                ++ans;
             return;
         }
 
         // Pick
         sub.push_back(s[i]);
-        solve(s, t, sub, ans, i+1);
+        solve(s, t, sub, ans, i + 1);
         sub.pop_back();
 
         // Dont Pick
-        solve(s, t, sub, ans, i+1);
+        solve(s, t, sub, ans, i + 1);
     }
 };
 
 int main()
 {
-    string s = "rabbbit", t = "rabbit" ;
+    string s = "babgbag", t = "bag";
 
-    Solution Obj1;
+    Solution1 Obj1;
     cout << Obj1.numDistinct(s, t);
 
     ios_base::sync_with_stdio(false);
