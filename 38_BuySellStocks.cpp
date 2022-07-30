e90c09486c8ad3e29908cea343d593d82e3da196
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-30 19:15:45 +0530

    TLE

---
+++ 38_BuySellStocks.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// You are given an array prices where prices[i] is the price of a 
+// given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy 
+// one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. 
+// If you cannot achieve any profit, return 0.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    // BruteForce : TLE 
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.size()-1; ++i)
+        {
+            for (int j = i+1; j < prices.size(); ++j)
+            {
+                profit = max(profit, (prices[j]-prices[i]));
+            }
+        }
+        return profit;
+    }
+};
+
+int main () {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
