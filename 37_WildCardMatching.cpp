de77a4a349488e3913b466ed8eb6f76b8a1ee67d
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-30 19:06:33 +0530

    WA

--- 37_WildCardMatching.cpp
+++ 37_WildCardMatching.cpp
@@ -10,6 +10,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class soluiton4
+{
+    // Tabulation: Wrong solution 
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length();
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        dp[0][0] = true;
+        for (int i = 0; i < n; ++i)
+        {
+            if (p[i] == '*') dp[0][i+1] = true;
+            else break;
+        }
+        
+        for (int i = 1; i <= m; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                if (s[i] == p[j] || p[j] == '?') dp[i][j] = dp[i-1][j-1];
+                else if (p[j] == '*') dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+                else dp[i][j] = false;
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
+
 class soluiton3
 {
     // Recursion: Memoization  
