119cf1b49806018d27e0ec71a4cd50ef3c37cdf8
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-09 21:05:35 +0530

    Solution3: Space Optimised

--- 40_BuySellStockIII.cpp
+++ 40_BuySellStockIII.cpp
@@ -3,6 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution3
+{
+    // Tabulation: Space Optimised 
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> curr(2, vector<int>(3, 0)), temp(2, vector<int>(3, 0));
+        for (int i = n-1; i >= 0; --i)
+        {
+            for (int buy = 0; buy < 2; ++buy)
+            {
+                for (int cap = 1; cap < 3; ++cap)
+                {
+                    int take, noTake;
+                    if (buy) {
+                        take = curr[0][cap] - prices[i];
+                        noTake = curr[1][cap];
+                    }
+                    else {
+                        take = curr[1][cap-1] + prices[i];
+                        noTake =curr[0][cap];
+                    }
+                    temp[buy][cap] = max(take, noTake);
+                }                
+            }
+            curr = temp;
+        }
+        
+        return curr[1][2];
+    }
+};
+
 class Solution2
 {
     // Tabulation 
@@ -32,26 +65,6 @@ public:
         }
         
         return dp[0][1][2];
-    }
-
-    int trade(vector<int> &prices, vector<vector<vector<int>>> &dp, int i, int buy, int cap = 2) {
-        // Base conditions 
-        if (i == prices.size()) return 0;
-        if (!cap) return 0;
-
-        if (dp[i][buy][cap] != -1) return dp[i][buy][cap];
-
-        int take, noTake;
-        if (buy) {
-            take = trade(prices, dp, i+1, false, cap) - prices[i];
-            noTake = trade(prices, dp, i+1, true, cap);
-        }
-        else {
-            take = trade(prices, dp, i+1, true, cap-1) + prices[i];
-            noTake = trade(prices, dp, i+1, false, cap);
-        }
-
-        return dp[i][buy][cap] = max(take, noTake);
     }
 };
 
---
+++ 41_BuySellStockIV.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+
+// You are given an integer array prices where prices[i] is the price of a 
+// given stock on the ith day, and an integer k.
+// Find the maximum profit you can achieve. You may complete at most 
+// k transactions.
+// Note: You may not engage in multiple transactions simultaneously 
+// (i.e., you must sell the stock before you buy again).
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+    }
+};
+
+int main () {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
