38975955efcfeeb03fc2ee20c7c1db95c16171bd
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-09 20:58:33 +0530

    SOlution2: Tabulation

--- 40_BuySellStockIII.cpp
+++ 40_BuySellStockIII.cpp
@@ -3,6 +3,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution2
+{
+    // Tabulation 
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for (int i = n-1; i >= 0; --i)
+        {
+            for (int buy = 0; buy < 2; ++buy)
+            {
+                for (int cap = 1; cap < 3; ++cap)
+                {
+                    int take, noTake;
+                    if (buy) {
+                        take = dp[i+1][0][cap] - prices[i];
+                        noTake = dp[i+1][1][cap];
+                    }
+                    else {
+                        take = dp[i+1][1][cap-1] + prices[i];
+                        noTake = dp[i+1][0][cap];
+                    }
+                    dp[i][buy][cap] = max(take, noTake);
+                }                
+            }
+        }
+        
+        return dp[0][1][2];
+    }
+
+    int trade(vector<int> &prices, vector<vector<vector<int>>> &dp, int i, int buy, int cap = 2) {
+        // Base conditions 
+        if (i == prices.size()) return 0;
+        if (!cap) return 0;
+
+        if (dp[i][buy][cap] != -1) return dp[i][buy][cap];
+
+        int take, noTake;
+        if (buy) {
+            take = trade(prices, dp, i+1, false, cap) - prices[i];
+            noTake = trade(prices, dp, i+1, true, cap);
+        }
+        else {
+            take = trade(prices, dp, i+1, true, cap-1) + prices[i];
+            noTake = trade(prices, dp, i+1, false, cap);
+        }
+
+        return dp[i][buy][cap] = max(take, noTake);
+    }
+};
+
 class Solution1
 {
     // Recursion: Memoization  
