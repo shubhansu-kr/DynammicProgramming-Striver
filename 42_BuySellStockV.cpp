dfa86ada8cd4d5d86c5f56785f70b6c43dc2bd69
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-10 21:27:56 +0530

    Solution8: Space Optimised

--- 42_BuySellStockV.cpp
+++ 42_BuySellStockV.cpp
@@ -12,6 +12,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution8 {
+    // Tabulation: SpaceOptimised 
+public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+
+        vector<int> A(2, 0), B(2, 0), C(2, 0);
+        for (int i = n-1; i >= 0; --i)
+        {
+            for (int stock = 0; stock < 2; ++stock)
+            {
+                int action = 0, noAction = B[stock];
+                if (stock) action = C[0] + prices[i];
+                else action = B[1] - prices[i];
+                A[stock] = max(action, noAction);
+            }
+            C = B, B = A;
+        }
+        
+        return A[0];
+    }
+};
+
 class Solution7 {
     // Tabulation 
 public:
