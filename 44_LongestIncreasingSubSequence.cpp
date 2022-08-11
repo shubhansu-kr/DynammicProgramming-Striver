0fa9f4a54c491117bcbede542108adadd06c0c0c
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-11 21:46:40 +0530

    Discussion Solution

--- 44_LongestIncreasingSubSequence.cpp
+++ 44_LongestIncreasingSubSequence.cpp
@@ -11,6 +11,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Discussion Solution
+// https://leetcode.com/problems/longest-increasing-subsequence/discuss/1326308/C++Python-DP-Binary-Search-BIT-Solutions-Picture-explain-O(NlogN)
+
+/* 
+class Solution8 { // 256 ms, faster than 42.84%
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
+class Solution7 { // 8 ms, faster than 91.61%
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the smallest number >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
+    }
+};
+
+class Solution6 {
+public:
+    vector<int> pathOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sub, subIndex; // Store index instead of value for tracing path purpose
+        vector<int> path(n, -1); // path[i] point to the index of previous number in LIS
+        for (int i = 0; i < n; ++i) {
+            if (sub.empty() || sub[sub.size() - 1] < nums[i]) {
+                path[i] = sub.empty() ? -1 : subIndex[sub.size() - 1];
+                sub.push_back(nums[i]);
+                subIndex.push_back(i);
+            } else {
+                int idx = lower_bound(sub.begin(), sub.end(), nums[i]) - sub.begin();
+                path[i] = idx == 0 ? -1 : subIndex[idx - 1];
+                sub[idx] = nums[i];
+                subIndex[idx] = i;
+            }
+        }
+        vector<int> ans;
+        int t = subIndex[subIndex.size() - 1];
+        while (t != -1) {
+            ans.push_back(nums[t]);
+            t = path[t];
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+class MaxBIT { // One-based indexing
+    vector<int> bit;
+public:
+    MaxBIT(int size) {
+        bit.resize(size + 1);
+    }
+    int get(int idx) {
+        int ans = 0;
+        for (; idx > 0; idx -= idx & -idx)
+            ans = max(ans, bit[idx]);
+        return ans;
+    }
+    void update(int idx, int val) {
+        for (; idx < bit.size(); idx += idx & -idx)
+            bit[idx] = max(bit[idx], val);
+    }
+};
+class Solution5 { // 16 ms, faster than 72.16%
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int BASE = 10001;
+        MaxBIT bit(20001);
+        for (int x : nums) {
+            int subLongest = bit.get(BASE + x - 1);
+            bit.update(BASE + x, subLongest + 1);
+        }
+        return bit.get(20001);
+    }
+};
+
+*/
+
+// My Solution: 
 class Solution4
 {
     // Tabulation: SpaceOptimisation 
