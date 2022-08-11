a7b154c6627af480d8a6d41e417b13277c974a01
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-11 22:30:09 +0530

    Solution1: Classic Dp

--- 45_PrintLIS.cpp
+++ 45_PrintLIS.cpp
@@ -3,6 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // Classic DP 
+public:
+    int LongestIncreasingSubSequence(vector<int> nums) {
+        int n = nums.size();
+        // dp[i] signifies the size of subsequece if ss ends at index i
+        vector<int> dp(n, 1);         
+        for (int i = 0; i < n; ++i)
+        {    
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[j]+1, dp[i]);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
 class Solution {
 public:
     int LongestIncreasingSubSequence(vector<int> nums) {
