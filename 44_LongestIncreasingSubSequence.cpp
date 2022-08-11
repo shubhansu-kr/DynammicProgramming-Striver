4c58d4d038fd5f3b9cf0eaf91ab2856e876ead8c
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-11 21:18:17 +0530

    Solution3: Tabulation

--- 44_LongestIncreasingSubSequence.cpp
+++ 44_LongestIncreasingSubSequence.cpp
@@ -11,6 +11,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution3
+{
+    // Tabulation 
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 0; j <= n; ++j)
+            {
+                int pick = -1e9;
+                if (j == 0 || nums[j-1] > nums[i-1]) {pick = 1 + dp[i-1][i];}
+                int noPick = dp[i-1][j];
+                dp[i][j] = max(pick, noPick);
+            }
+        }
+        return dp[n][0];
+    }
+};
+
 class Solution2
 {
     // Wrong Solution : Runtime error 
