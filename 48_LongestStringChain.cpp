782dd01eb4013e8d7e379df374cbcde198f541be
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-16 23:14:49 +0530

    Find the longest string chain

--- 48_LongestStringChain.cpp
+++ 48_LongestStringChain.cpp
@@ -14,7 +14,33 @@ using namespace std;
 
 class Solution
 {
-    // TLE: Time Limit Exceeded 
+public:
+    static bool compare(const string &s1, const string &s2)
+    {
+        return s1.length() < s2.length();
+    }
+
+    int longestStrChain(vector<string> &words)
+    {
+        sort(words.begin(), words.end(), compare);
+        unordered_map<string, int> dp;
+        int res = 0;
+        for (string w : words)
+        {
+            for (int i = 0; i < w.length(); i++)
+            {
+                string pre = w.substr(0, i) + w.substr(i + 1);
+                dp[w] = max(dp[w], dp.find(pre) == dp.end() ? 1 : dp[pre] + 1);
+            }
+            res = max(res, dp[w]);
+        }
+        return res;
+    }
+};
+
+class Solution
+{
+    // TLE: Time Limit Exceeded
 public:
     bool compare(string s, string t)
     {
